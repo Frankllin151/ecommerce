e728576157c2ff90f3b4cec8a6e070630fbd3086
Frankllin151 <frankllinsilva300@gmail.com>
2024-01-26 09:17:27 -0300

    style:product

--- assets/js/component.js
+++ assets/js/component.js
@@ -1,6 +1,10 @@
 function IncludeComponets() {
    const MOdal = document.getElementById("MODAL");
     const header = document.getElementById("header");
+    const homebanner = document.getElementById("BANNERHOME");
+    const about = document.getElementById("ABOUT");
+    const exclusivo  = document.getElementById("EXCLUSIVO");
+    const products  = document.getElementById("PRODUCTS");
 
     fetch("/components/modal.html")
     .then(response => response.text())
@@ -9,6 +13,23 @@ function IncludeComponets() {
     fetch("/components/header.html")
     .then(response => response.text())
     .then(data => header.innerHTML = data);
+
+    fetch("/components/banner.html")
+    .then(response => response.text())
+    .then(data => homebanner.innerHTML = data);
+
+    fetch("/components/about.html")
+    .then(response => response.text())
+    .then(data => about.innerHTML = data);
+
+    fetch("/components/exclusivo.html")
+    .then(response => response.text())
+    .then(data => exclusivo.innerHTML = data);
+
+
+    fetch("/components/products.html")
+    .then(response => response.text())
+    .then(data => products.innerHTML = data);
 
    
 
